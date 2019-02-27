9399d1e6fe7bf105abaf371c95f9812ed7c79eea
yusufalicezik <yusufalicezik35@gmail.com>
2019-02-28 00:22:09 +0300

    randevuSt

--- app/src/main/java/com/yusufalicezik/drvirtual/HastaIslemleri/RandevuAlActivity.java
+++ app/src/main/java/com/yusufalicezik/drvirtual/HastaIslemleri/RandevuAlActivity.java
@@ -1,15 +1,18 @@
 package com.yusufalicezik.drvirtual.HastaIslemleri;
 
 import android.app.DatePickerDialog;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -18,9 +21,15 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.yusufalicezik.drvirtual.R;
+import com.yusufalicezik.drvirtual.Utils.DoktorCalismaTarihiParcala;
 
+import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
 
 public class RandevuAlActivity extends AppCompatActivity {
@@ -28,8 +37,18 @@ public class RandevuAlActivity extends AppCompatActivity {
 
     private Spinner hastaneSecSpinner, bolumSecSpinner, doktorSecSpinner;
     private EditText secilenTarih;
+    private LinearLayout randevuTarihLayout;
+    private Button r08,r10,r11,r12,r13,r15,r17;
+
+    private Button []randevuButonlari;
+
 
     private int anaId=0;
+
+
+    private int secilenGun=0;
+
+
 
     private ArrayList<String> hastaneler=new ArrayList<>();
     private ArrayList<Integer> hastenelerID=new ArrayList<>();
@@ -53,6 +72,13 @@ public class RandevuAlActivity extends AppCompatActivity {
 
     private ArrayAdapter<String> adapterHastane;
 
+
+    ///Static
+    public static HashMap<Integer, String> mapGunler=new HashMap<>();
+    public static HashMap<Integer, String> mapSaatler=new HashMap<>();
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,6 +87,39 @@ public class RandevuAlActivity extends AppCompatActivity {
         doktorSecSpinner=findViewById(R.id.spn_DoktorSecim);
         bolumSecSpinner=findViewById(R.id.spn_BolumSecim);
         secilenTarih=findViewById(R.id.edt_secilenTarih);
+        randevuTarihLayout=findViewById(R.id.randevuTarihGenelLayout);
+
+        randevuTarihLayout.setVisibility(View.INVISIBLE);
+
+        r08=findViewById(R.id.button_randevu08);
+        r10=findViewById(R.id.button_randevu10);
+        r11=findViewById(R.id.button_randevu11);
+        r12=findViewById(R.id.button_randevu12);
+        r13=findViewById(R.id.button_randevu13);
+        r15=findViewById(R.id.button_randevu15);
+        r17=findViewById(R.id.button_randevu17);
+
+        randevuButonlari= new Button[]{r08, r10,r11,r12,r13,r15,r17};
+
+
+
+        mapGunler.put(1,"Pazartesi");
+        mapGunler.put(2,"Salı");
+        mapGunler.put(3,"Çarşamba");
+        mapGunler.put(4,"Perşembe");
+        mapGunler.put(5,"Cuma");
+        mapGunler.put(6,"Cumartesi");
+        mapGunler.put(7,"Cumartesi");
+
+        mapSaatler.put(1,"08:00");
+        mapSaatler.put(2,"10:00");
+        mapSaatler.put(3,"11:00");
+        mapSaatler.put(4,"12:00");
+        mapSaatler.put(5,"13:00");
+        mapSaatler.put(6,"15:00");
+        mapSaatler.put(7,"17:00");
+
+
 
         adapterHastane= new ArrayAdapter<String>(getApplicationContext(),
                 android.R.layout.simple_spinner_item, hastaneler);
@@ -228,9 +287,14 @@ public class RandevuAlActivity extends AppCompatActivity {
         final Calendar takvim = Calendar.getInstance();
         int yil = takvim.get(Calendar.YEAR);
         int ay = takvim.get(Calendar.MONTH);
-        int gun = takvim.get(Calendar.DAY_OF_MONTH);
+        int gun = takvim.get(Calendar.DAY_OF_MONTH)+1;
+
+
+
+
 
         DatePickerDialog dpd = new DatePickerDialog(RandevuAlActivity.this, new DatePickerDialog.OnDateSetListener() {
+
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 // ay değeri 0 dan başladığı için (Ocak=0, Şubat=1,..,Aralık=11)
@@ -239,6 +303,16 @@ public class RandevuAlActivity extends AppCompatActivity {
                 // year, month ve dayOfMonth değerleri seçilen tarihin değerleridir.
                 // Edittextte bu değerleri gösteriyoruz.
                 secilenTarih.setText(dayOfMonth + "/" + month + "/" + year);
+
+
+
+
+                GregorianCalendar GregorianCalendar = new GregorianCalendar(year, month-1, dayOfMonth-1);
+
+                int dayOfWeek=GregorianCalendar.get(takvim.DAY_OF_WEEK);
+
+                secilenGun=dayOfWeek;
+
             }
         }, yil, ay, gun);
       // datepicker açıldığında set edilecek değerleri buraya yazıyoruz.
@@ -246,8 +320,111 @@ public class RandevuAlActivity extends AppCompatActivity {
         // dialog penceresinin button bilgilerini ayarlıyoruz ve ekranda gösteriyoruz.
         dpd.setButton(DatePickerDialog.BUTTON_POSITIVE, "Seç", dpd);
         dpd.setButton(DatePickerDialog.BUTTON_NEGATIVE, "İptal", dpd);
-        dpd.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+        dpd.getDatePicker().setMinDate(System.currentTimeMillis()+86400000);
+        dpd.getDatePicker().setMaxDate(System.currentTimeMillis()+604800000);
+
+
+
+
         dpd.show();
+
+    }
+    public void randevuAlButonTik(View view){
+
+        String denemeVeri="1-3,1-5,1-1,2-1,1-4,2-2,2-4,2-5"; //dolu olanlar bunlar. Bunlar bir doktorun(seçilen) doktorun yani
+        //daha önce alınan randevuları.. günler ve saatlere göre..
+
+        DoktorCalismaTarihiParcala doktorCalismaTarihiParcala=new DoktorCalismaTarihiParcala();
+
+        ArrayList<ArrayList>gelenListe;
+
+        gelenListe=doktorCalismaTarihiParcala.parcala(denemeVeri);
+
+        ArrayList<String>doluGunler=gelenListe.get(0);
+        ArrayList<String>doluSaatler=gelenListe.get(1);
+
+        for(int i=0;i<doluGunler.size();i++){
+
+            Log.e("kontrol", "Dolu Gün : "+mapGunler.get(Integer.valueOf(doluGunler.get(i))) + " ve saati : "+
+                    mapSaatler.get(Integer.valueOf(doluSaatler.get(i))));
+
+        }
+
+        Log.e("secilenGun",String.valueOf(secilenGun));
+
+
+        ArrayList<String>secilenGunDoluSaatler=new ArrayList<>();
+        for(int i=0;i<doluGunler.size();i++){
+            if(doluGunler.get(i).equals(String.valueOf(secilenGun))){
+                secilenGunDoluSaatler.add(doluSaatler.get(i));
+
+            }
+        }
+
+
+
+        ////
+        randevuTarihLayout.setVisibility(View.VISIBLE);
+
+
+
+
+        /////T
+        for(int i=0;i<randevuButonlari.length;i++){
+            randevuButonlari[i].setEnabled(true);
+            randevuButonlari[i].setBackgroundColor(Color.GREEN);
+
+            final int finalI = i;
+            randevuButonlari[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(RandevuAlActivity.this, randevuButonlari[finalI].getText().toString()+
+                            " saatini seçtiniz..", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        /////T
+
+        for(int i=0;i<secilenGunDoluSaatler.size();i++){
+            Log.e("dols",secilenGunDoluSaatler.get(i));
+      // Log.e("doluu",mapSaatler.get(Integer.valueOf(secilenGunDoluSaatler.get(i))));
+           String saat=mapSaatler.get(Integer.valueOf(secilenGunDoluSaatler.get(i)));
+           switch (saat){
+               case "08:00":{
+                   randevuButonlari[0].setEnabled(false);
+                   randevuButonlari[0].setBackgroundColor(Color.RED);
+               }break;
+               case "10:00":{
+                   randevuButonlari[1].setEnabled(false);
+                   randevuButonlari[1].setBackgroundColor(Color.RED);
+               }break;
+               case "11:00":{
+                   randevuButonlari[2].setEnabled(false);
+                   randevuButonlari[2].setBackgroundColor(Color.RED);
+               }break;
+               case "12:00":{
+                   randevuButonlari[3].setEnabled(false);
+                   randevuButonlari[3].setBackgroundColor(Color.RED);
+               }break;
+               case "13:00":{
+                   randevuButonlari[4].setEnabled(false);
+                   randevuButonlari[4].setBackgroundColor(Color.RED);
+               }break;
+               case "15:00":{
+                   randevuButonlari[5].setEnabled(false);
+                   randevuButonlari[5].setBackgroundColor(Color.RED);
+               }break;
+               case "17:00":{
+                   randevuButonlari[6].setEnabled(false);
+                   randevuButonlari[6].setBackgroundColor(Color.RED);
+               }break;
+           }
+        }
+
+
+
+        //BU KISIMLAR FARKLI METHODTA YAPILABİLİR.
+
 
     }
 }
